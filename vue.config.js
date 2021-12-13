95143e1bb3f8926b20e3d417f908c8419c7ac792
2019211597 卓睿 <1075860716@qq.com>
2021-12-13 13:52:14 +0800

    📃 docs: Useless comment

--- vue.config.js
+++ vue.config.js
@@ -6,12 +6,7 @@ module.exports = {
     configureWebpack: {
         plugins: [
             new MonacoEditorPlugin({
-                // https://github.com/Microsoft/monaco-editor-webpack-plugin#options
-                // Include a subset of languages support
-                // Some language extensions like typescript are so huge that may impact build performance
-                // e.g. Build full languages support with webpack 4.0 takes over 80 seconds
-                // Languages are loaded on demand at runtime
-                languages: ["typescript", "css", "html", "typescript"],
+                languages: ["javascipt", "css", "html", "typescript"],
             }),
         ],
     },
